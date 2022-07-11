1a5c1d7c0869d2c06543c927070a4c65f4bac8a7
JershDervis <joshdavis16@outlook.com>
2022-07-11 21:45:55 +1000

    Netlify deploy issues

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,16 +11,7 @@ const config = {
     }),
   ],
 	kit: {
-		adapter: adapter({
-      // if true, will create a Netlify Edge Function rather
-      // than using standard Node-based functions
-      edge: true,
-
-      // if true, will split your app into multiple functions
-      // instead of creating a single one for the entire app.
-      // if `edge` is true, this option cannot be used
-      split: false
-    })
+		adapter: adapter()
 	}
 };
 
